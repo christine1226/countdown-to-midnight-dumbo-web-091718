45d36dfe8c6c0bc783f477fad9ad6eafcb2790ec
christine gomez <christine.gomez1226@gmail.com>
2018-08-28 23:18:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(10)
+  x = 10
+  while x > 0
+    puts "#{x} SECONDS!"
+    x -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
 end
